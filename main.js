cc8baae3e3c49b0ebcb8c6e85c7e91718837f40a
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-02-01 19:55:04 -0300

    Added offSet.

--- main.js
+++ main.js
@@ -29,41 +29,38 @@ function drawTexture(
 		"void Draw(Texture2D texture, Vector2 position, Nullable`1 sourceRectangle, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth)"
 	](texture, position, null, color, rotation, origin, scale, sprite, 0.0);
 }
-const getOrigin = (x, y) => Vector2.new()['void .ctor(float x, float y)'](x / 2, y / 2);
+const getOrigin = (x, y) => vector2(x / 2, y / 2);
 const Subtract = Vector2['Vector2 Subtract(Vector2 value1, Vector2 value2)']
 const Normalize = Vector2['Vector2 Normalize(Vector2 value)']
 
 // end
 
-Main.DrawInterface_FinalUI.hook((orig, self) => {
-    orig(self)
-});
-
 Main.DrawRain.hook((orig, seld) => {
-    
-    // Draw after UI.
-    orig(seld)
-    
-    
-    const player = Main.player[0]
-    
+    orig(seld);
+
+    const player = Main.player[0];
+
     const cursorX = Main.mouseX + Main.screenPosition.X;
     const cursorY = Main.mouseY + Main.screenPosition.Y;
-    const cursorPosition = Vector2.new()['void .ctor(float x, float y)'](cursorX, cursorY)
-    
-    let rotation = Utils.ToRotation(Subtract(player.Center, cursorPosition)) - Math.PI * 2
-    let tangent = Normalize (Subtract(player.Center, cursorPosition))
-    let screenPosition = Subtract(tangent, Main.screenPosition)
-    
-        drawTexture
-            (
-                cursor, //
-                screenPosition,
-                Color.White,
-                rotation,
-                getOrigin(cursor.Width, cursor.Height),
-                1
-            )
+    const cursorPosition = vector2(cursorX, cursorY);
+
+    let direction = Subtract(cursorPosition, player.Center);
+    let rotation = Utils.ToRotation(direction) - Math.PI * 2;
+
+    let normalizedDir = Normalize(direction);
+
+    let offsetDistance = 20;
+    let offset = vector2(normalizedDir.X * offsetDistance, normalizedDir.Y * offsetDistance);
+
+    let screenPosition = Subtract(player.Center, Main.screenPosition);
+    screenPosition = vector2(screenPosition.X + offset.X, screenPosition.Y + offset.Y);
+
+    drawTexture(
+        cursor,
+        screenPosition,
+        Color.White,
+        rotation,
+        getOrigin(cursor.Width, cursor.Height),
+        1
+    );
 });
-    
-    
